e249fcf7074873f8bc4e267d87ebfa3217e78b0f
abdullah salameh <abdullahsalameh66@gmail.com>
2025-12-28 07:10:31 +0300

    Deploy to gh-pages - 12/28/2025 7:10:31 AM

--- out/_next/static/chunks/app/boards/[board]/page-230c64e4d683f2e7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[910,5112,7220,7867],{64771:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,48173,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[8173,8441,1517,7358],()=>s(64771)),_N_E=e.O()}]);
