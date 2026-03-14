5d915c67541ee829f1026d10ed6de758c184f61b
Karim Alkam <karimalkamht@gmail.com>
2026-03-15 02:34:10 +0300

    Update content - 2026-03-15 02:34

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const nextConfig = {
   output: 'export', // Enable static HTML export
-  basePath: 'fucking-blog', // Replace with your GitHub repo name if deploying under a subpath
+  basePath: '/fucking-blog', // Replace with your GitHub repo name if deploying under a subpath
   assetPrefix: '',
   trailingSlash: true, // Required for GitHub Pages to handle static routes
 };
